60fb1766371472422b8a76630d3febc9b00dbdbe
Benjamin Dallard <dallardbenj@gmail.com>
2019-04-30 23:04:48 +0200

    Update test0_OSC.py

--- osc/test0_OSC.py
+++ osc/test0_OSC.py
@@ -1,5 +1,6 @@
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
+import numpy as np 
 rdm = np.random.dirichlet(np.ones(7),size=1)
 client = udp_client.UDPClient('127.0.0.1', 10042)
 msg = osc_message_builder.OscMessageBuilder(address = list(rdm.tolist()))
