712bd2b4f5a5b6bee7b78d48a07183f4c78b01be
Benjamin Dallard <dallardbenj@gmail.com>
2019-05-01 03:09:51 +0200

    update frequency

--- osc/test0_OSC.py
+++ osc/test0_OSC.py
@@ -5,11 +5,11 @@ import numpy as np
 client=udp_client.UDPClient('127.0.0.1', 10042)
 
 for i in range(10):
+    np.random.randint(10042)
     rdm=np.random.dirichlet(np.ones(7),size=1)
-    n=[x for x in rdm.tolist()[0]]
-    m=str(n)[1:-1]
+    m=str([x for x in rdm.tolist()[0]])[1:-1].replace(",","")
     msg=osc_message_builder.OscMessageBuilder(address=m)
-    msg.add_arg('message ok')
+    msg.add_arg('unpacked ok')
     msg=msg.build()
     client.send(msg)
 
